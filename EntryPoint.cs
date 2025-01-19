296b77abe4f66a3416fcdbae54faf4fad6b83fa3
Pietro Terracciano <pterracciano95@gmail.com>
2025-01-19 13:03:22 +0100

    Commit

---
+++ EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Kudos.DataBasing.UnitTest
+{
+    public class EntryPoint
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
